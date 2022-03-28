08cd94badb6c18b83ce9e4df7ea234b7a2dd29e2
JoonasK96 <joonas.kelo@metropolia.fi>
2022-03-28 10:38:13 +0300

    lisätty feed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Profile from './Pages/Profile'
 import Login from './Pages/Login'
 import PostQuestion from './Pages/postQuestion'
 import PostAnswer from './Pages/postAnswer'
+import Feed from './Pages/Feed'
 
 function App() {
 return (
       <Router>
         <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/postQuestion" element={<PostQuestion />} />
         <Route path="/postAnswer" element={<PostAnswer />} />
