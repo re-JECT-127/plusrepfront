ddc3e9bfa395fa651e7cb506ffd890b954e0256f
BuildTools <ollipallo98@gmail.com>
2022-04-12 14:06:39 +0300

    Removed Bootstrap CSS
    It was garbage and broke everything

--- src/Pages/postQuestion.js
+++ src/Pages/postQuestion.js
@@ -94,19 +94,14 @@ function PostQuestion({setOpenModal}) {
       <div className="modalContainer">
       <Notification message={notification} />
       <Error message={error} />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossOrigin="anonymous"
-      />
+
       <link rel="stylesheet" href="profile.css" />
-      <div className="container bootstrap snippets bootdey">
-        <div className="row">
-          <div className="col-md-offset-3 col-md-6 col-xs-12">
-            <div className="well well-sm well-social-post">
-              <ul className="list-inline" id="list_PostActions">
-                <li className="active">
+      <div>
+        <div>
+          <div>
+            <div>
+              <ul>
+                <li>
                   <div>
                     <input type="file" name="file" onChange={changeHandler} />
 
@@ -128,18 +123,19 @@ function PostQuestion({setOpenModal}) {
                 </li>
               </ul>
               
-              <div className="postquestion-textform">
+              <div className="postquestion-textform" display='flex' flex-direction = 'row'>
                 <TextForm
                   onSubmit={addPost}
                   postValue={newPost}
                   postChange={handlePostChange}
                 />
-              </div>
-              <button onClick={() => {
+                <button class= 'btn' onClick={() => {
               setOpenModal(false);
               }} id="cancelBtn">
               Cancel
               </button>
+              </div>
+              
               <ul className="list-inline post-actions">
                 <li>
                   <a href="#">
